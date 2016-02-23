990d948630771c9d8bb324ca9b0ecde68cc8f9f0
bobashev <bobashev@rti.org>
2016-02-23 14:45:28 -0500

    Added files via upload

---
+++ SVM.R
@@ -0,0 +1,21 @@
+# SVM Application to Gene Expression Data
+install.packages("ISLR")
+library(ISLR)
+library(MASS)
+library(e1071)
+?Khan
+names(Khan)
+dim(Khan$xtrain)
+dim(Khan$xtest)
+length(Khan$ytrain)
+length(Khan$ytest)
+table(Khan$ytrain)
+table(Khan$ytest)
+dat=data.frame(x=Khan$xtrain, y=as.factor(Khan$ytrain))
+out=svm(y~., data=dat, kernel="linear",cost=10)
+summary(out)
+table(out$fitted, dat$y)
+dat.te=data.frame(x=Khan$xtest, y=as.factor(Khan$ytest))
+pred.te=predict(out, newdata=dat.te)
+table(pred.te, dat.te$y)
+
